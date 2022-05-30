d33926d0f28a932136a46c61a8c7f39564b30ac7
IvanKotykUkr <iwankot3@gmail.com>
2022-05-30 20:38:07 +0100

    add js

---
+++ index.js
@@ -0,0 +1,32 @@
+var x;
+(function (x) {
+    x["inst"] = "jdcsnofndsjo";
+    x["vk"] = "sakdmkasmnd";
+    x["fcb"] = "dsfdsf";
+})(x || (x = {}));
+var createPasword = function (name, age) { return '$(name)$(age)'; };
+var user = {
+    name: 'misha',
+    age: 22,
+    nickName: 'fghvhgnvghj'
+};
+var admin = {
+    name: 'misha',
+    age: 22,
+    getPass: 324234
+};
+var castomer = {
+    name: 'misha',
+    age: 22,
+    nickName: 'fghvhgnvghj'
+};
+var User = /** @class */ (function () {
+    function User(name, age, nickName) {
+        this.name = name;
+        this.age = age;
+        this.nickName = nickName;
+    }
+    return User;
+}());
+var ihor = new User('ihor', 44, 'csvs');
+console.log(ihor);
