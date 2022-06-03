e0b06dda35ad4481086b79672e1888e7072c6a49
IvanKotykUkr <iwankot3@gmail.com>
2022-06-03 11:42:16 +0100

    add js

--- testfunction.js
+++ testfunction.js
@@ -14,14 +14,109 @@ if (){
 
 } else {
 
-}}*/
-var message = 'Привет ';
-var messageAnd = (5 > 9) ? 'vasya' : 'olya';
-message += messageAnd;
-console.log(message);
-if (0) {
-    console.log('Fake');
+}}
+let message:string='Привет ';
+
+
+let messageAnd:string = (5>9)?'vasya':'olya';
+message+=messageAnd;
+console.log(message)
+
+if(0){
+    console.log('Fake')
+} else {
+    console.log('True')
 }
-else {
-    console.log('Truu');
+
+function calcSum(numOne:number,numTwo:number) {
+    let result:number=1;
+    for (let i:number=0;i<numTwo;i++){
+        result *= numOne;
+
+    }
+
+    return result;
+
+console.log(result);
 }
+
+calcSum(2,3)
+
+
+let showNumber = (num:number) => {
+    console.log(num);
+    let timeId:number = setTimeout(showNumber,1000,++num)
+    if (num>=10){
+        clearTimeout(timeId)
+    }
+
+
+}
+setTimeout(showNumber,1000,1)
+let MessSage:any=ShowMessage();
+if(2>1) {
+    function ShowMessage() {
+        console.log('Message');
+
+    }
+
+}
+
+MessSage();
+
+
+let userInfo:{name:string,age:number,'likesw':boolean} = {
+    name:"vasya",
+    age: 30,
+    "likesw":true,
+};
+
+let key:string = 'name';
+
+
+
+
+let userid:{name:string,age:number,adress:{city:string,streat:string}}= {
+    name:'vasya',
+    age:33,
+    adress:{
+        city:'lviv',
+        streat:"central",
+    }
+
+};
+
+
+function makeUserId(name:string,age:number){
+    return{
+        name:name,
+        age:age,
+    };
+}
+let user=makeUserId('Vasya',44)
+
+let userAdd:{name?:string,age?:number,adress?:{city:string,streat:string}}={
+    name:"ihor",
+};
+
+
+userAdd.age=44;
+
+
+userAdd.adress = {
+    city:'dfwsd',
+    streat:"dsfds",
+}
+var userid = {
+    name: 'vasya',
+    age: 33,
+    adress: {
+        city: 'lviv',
+        streat: "central"
+    },
+    showinfo: function () {
+        console.log((this.name), (this.age), 'містою ', (this.adress));
+    }
+};
+userid.showinfo();
+*/
--- testfunction.ts
+++ testfunction.ts
@@ -14,7 +14,7 @@ if (){
 
 } else {
 
-}}*/
+}}
 let message:string='Привет ';
 
 
@@ -27,3 +27,95 @@ if(0){
 } else {
     console.log('True')
 }
+
+function calcSum(numOne:number,numTwo:number) {
+    let result:number=1;
+    for (let i:number=0;i<numTwo;i++){
+        result *= numOne;
+
+    }
+
+    return result;
+
+console.log(result);
+}
+
+calcSum(2,3)
+
+
+let showNumber = (num:number) => {
+    console.log(num);
+    let timeId:number = setTimeout(showNumber,1000,++num)
+    if (num>=10){
+        clearTimeout(timeId)
+    }
+
+
+}
+setTimeout(showNumber,1000,1)
+let MessSage:any=ShowMessage();
+if(2>1) {
+    function ShowMessage() {
+        console.log('Message');
+
+    }
+
+}
+
+MessSage();
+
+
+let userInfo:{name:string,age:number,'likesw':boolean} = {
+    name:"vasya",
+    age: 30,
+    "likesw":true,
+};
+
+let key:string = 'name';
+
+
+
+
+let userid:{name:string,age:number,adress:{city:string,streat:string}}= {
+    name:'vasya',
+    age:33,
+    adress:{
+        city:'lviv',
+        streat:"central",
+    }
+
+};
+
+
+function makeUserId(name:string,age:number){
+    return{
+        name:name,
+        age:age,
+    };
+}
+let user=makeUserId('Vasya',44)
+
+let userAdd:{name?:string,age?:number,adress?:{city:string,streat:string}}={
+    name:"ihor",
+};
+
+
+userAdd.age=44;
+
+
+userAdd.adress = {
+    city:'dfwsd',
+    streat:"dsfds",
+
+let userid:{name:string,age:number,adress:{city:string,streat:string},showinfo:any}= {
+    name:'vasya',
+    age:33,
+    adress:{
+        city:'lviv',
+        streat:"central",
+    },
+    showinfo (){
+        console.log((this.name),(this.age),'містою ',(this.adress ));
+    }
+}
+userid.showinfo();*/
