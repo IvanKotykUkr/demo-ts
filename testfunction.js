5cf9ad1db12e9e1b226121a1cd94346d4bc04487
IvanKotykUkr <iwankot3@gmail.com>
2022-06-03 08:40:10 +0100

    add js

--- testfunction.js
+++ testfunction.js
@@ -1,7 +1,27 @@
-function test(name, age) {
-    console.log(name, age);
-}
-test('ihor', 33);
+/*let num:number=0;
+while (num<3){
 
-var ihor = require('./index');
-console.log(ihor);
+    console.log('Number num: '+ num);
+    num++;
+
+Finish: for (num = 0;num<2;num++){
+    for (let size:number=0;size<3;size++ ){
+        if (size==2) break Finish;
+        console.log(size);
+    }
+}
+if (){
+
+} else {
+
+}}*/
+var message = 'Привет ';
+var messageAnd = (5 > 9) ? 'vasya' : 'olya';
+message += messageAnd;
+console.log(message);
+if (0) {
+    console.log('Fake');
+}
+else {
+    console.log('Truu');
+}
--- testfunction.ts
+++ testfunction.ts
@@ -1,9 +1,29 @@
+/*let num:number=0;
+while (num<3){
 
+    console.log('Number num: '+ num);
+    num++;
 
-function test(name:string,age:number) {
-    console.log(name,age);
-
+Finish: for (num = 0;num<2;num++){
+    for (let size:number=0;size<3;size++ ){
+        if (size==2) break Finish;
+        console.log(size);
+    }
 }
+if (){
 
-test('ihor',33)
+} else {
 
+}}*/
+let message:string='Привет ';
+
+
+let messageAnd:string = (5>9)?'vasya':'olya';
+message+=messageAnd;
+console.log(message)
+
+if(0){
+    console.log('Fake')
+} else {
+    console.log('True')
+}
